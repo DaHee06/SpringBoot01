148912b6088cfe8732b4d12a67fd5672eb1cd5e9
KimCome <todayda1006@gmail.com>
2023-08-14 23:12:40 +0900

    Initial commit

---
+++ src/main/java/com/example/LoggingRunner.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoggingRunner implements ApplicationRunner {
+
+
+    private Logger logger = LoggerFactory.getLogger(LoggingRunner.class);
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        logger.trace("TRACE 로그 테스트");
+        logger.debug("DEBUG 로그 테스트");
+        logger.info("INFO 로그 테스트");
+        logger.warn("WARN 로그 테스트");
+        logger.error("ERROR 로그 테스트");
+    }
+}
